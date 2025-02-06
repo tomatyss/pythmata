6d833902af77d23c5795d858a9ffaf06de42fb80
ivan <tomatyss@gmail.com>
2025-02-06 16:51:50 -0500

    fix: add global TextEncoder and TextDecoder for testing compatibility

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,4 +1,8 @@
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'node:util';
+
+global.TextEncoder = TextEncoder as typeof global.TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
 
 // Mock matchMedia for components that might use it
 Object.defineProperty(window, 'matchMedia', {
